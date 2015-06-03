dc8143f9d0e2bfdd45b0fb6e11a5dfaeb661f787
jingrouliao1996 <981932631@qq.com>
2015-06-03 17:07:45 +0800

    my_stack

--- final-test/c_cpp/my_stack.c
+++ final-test/c_cpp/my_stack.c
@@ -1,8 +1,32 @@
-/*
- * my_stack的实现文件
- * 如果采用C++的实现，那也可以把此文件名改为.cpp
- * 但是记住g++才是编译C++的编译器
- *
- */
+#include <stdio.h>
+
+#include "include/my_stack.h"
+
+int num = 0;
+
+int a[5];
+
+int stack_is_full() {
+        if(num == 5)
+            return 1;
+        else 
+            return 0;
+	}
+
+int stack_is_empty() {
+        if(num == 0)
+            return 1;
+        else
+            return 0;
+	}
 
 
+void stack_push(int x) {
+        a[num]= x ;
+        num++;
+	}
+
+int stack_pop(){
+        num--;
+        return a[num];
+        }
